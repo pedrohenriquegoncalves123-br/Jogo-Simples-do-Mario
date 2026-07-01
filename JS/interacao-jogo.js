f92f8872137ac33dc81370df72d272f0d5caee50
pedrohenriquegoncalves123-br <pedrohenriquegoncalves564@gmail.com>
2026-07-01 17:58:14 -0300

    Update Mario game interaction and collision logic
    Refactor game interaction logic and add collision detection.

--- JS/interacao-jogo.js
+++ JS/interacao-jogo.js
@@ -1,20 +1,23 @@
 const mario = document.querySelector(".mario");
-const tubo = document.querySelector(".tubo");
 const container = document.querySelector(".container");
+const pipe = document.querySelector(".tubo");
 
-window.addEventListener("keydown", (pular) => {
+window.addEventListener("keydown", () => {
     mario.classList.add("jump");
 
     setTimeout(() => {
         mario.classList.remove("jump");
     }, 1500);
 
-    // Calcula o bottom atual
-    const posicao = tubo.getBoundingClientRect();
-    console.log(posicao.bottom);
-
-    if (tubo) {
-
-    }
 })
 
+const loop = setInterval(() => {
+    let pipePosicao = Number(pipe.offsetLeft);
+
+    let marioPosicao = Number(mario.offsetBottom);
+
+    if (pipePosicao < 140 || marioPosicao <= 85) {
+        mario.src = "imagens/mario-game-over.jpg";
+        pipe.classList.remove("pipe-slide");
+    }
+}, 10);
