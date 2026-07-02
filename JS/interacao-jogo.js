487c9e7502bc02783f17865243d6b52a86d8e7e4
pedrohenriquegoncalves123-br <pedrohenriquegoncalves564@gmail.com>
2026-07-02 19:21:18 -0300

    Update interacao-jogo.js

--- JS/interacao-jogo.js
+++ JS/interacao-jogo.js
@@ -1,23 +1,60 @@
 const mario = document.querySelector(".mario");
+const placar = document.querySelector(".game-placar");
+let canoPassou = false;
 const container = document.querySelector(".container");
+
 const pipe = document.querySelector(".tubo");
 
+
+
 window.addEventListener("keydown", () => {
+
     mario.classList.add("jump");
 
     setTimeout(() => {
         mario.classList.remove("jump");
     }, 1500);
 
-})
+});
+
+
+
+const playAgain = document.querySelector(".btn-play-again");
 
 const loop = setInterval(() => {
     let pipePosicao = Number(pipe.offsetLeft);
 
-    let marioPosicao = Number(mario.offsetBottom);
+    let marioPosicao = +window.getComputedStyle(mario).bottom.replace('px', '');
+    if (pipePosicao < 120 && marioPosicao <= 70) {
 
-    if (pipePosicao < 140 || marioPosicao <= 85) {
-        mario.src = "imagens/mario-game-over.jpg";
-        pipe.classList.remove("pipe-slide");
+        pipe.style.animation = "none";
+        mario.classList.remove("jump");
+        pipe.style.left = `${pipePosicao}px`;
+
+        mario.style.bottom = `${marioPosicao}px`;
+
+        mario.src = "./imagens/mario-game-over.jpg";
+        mario.style.width = "120px";
+        playAgain.classList.add("hidden-button");
+    }
+
+    const contador = document.getElementById("contador")
+    if (pipePosicao < 0) {
+
+        contador.innerHTML = placarJogo;
+    }
+
+    if (pipePosicao < 30 && pipePosicao > 0 && marioPosicao > 70) {
+        if (!canoPassou) {
+            placarJogo++;
+            contador.innerHTML = placarJogo;
+            canoPassou = true;
+        }
     }
 }, 10);
+
+
+playAgain.addEventListener("click", () => {
+    location.reload(true);
+
+})
